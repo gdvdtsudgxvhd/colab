a389ae57325551a9fc76d296c70c71f6a06cd72e
pycharm <fuhitozz@gmail.com>
2024-12-03 10:28:39 +0800

    init

---
+++ colab/ui/render_html.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+def render_template(filename, params):
+    with open(f"{Path(__file__).resolve().parent}/{filename}") as f:
+        template = f.read()
+        return template.format(**params)
